fd271aabd17e3f09be42c68d6d3d35287cc57f15
batherit <batherit0703@naver.com>
2020-06-06 21:57:35 +0900

    [ADD_SJH] 카메라 컨트롤러 추가

---
+++ TeamProject/Assets/SJH/Scripts/SC_CameraControl.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_CameraControl : MonoBehaviour
+{
+    public float m_DampTime = 0.2f;
+    public float m_ScreenEdgeBuffer = 4f;
+    public float m_MinSize = 6.5f;
+    /*[HideInInspector] */public Transform[] m_Targets;
+
+
+    private Camera m_Camera;
+    private float m_ZoomSpeed;
+    private Vector3 m_MoveVelocity;
+    private Vector3 m_DesiredPosition;
+
+
+    private void Awake()
+    {
+        m_Camera = GetComponentInChildren<Camera>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
+        Zoom();
+    }
+
+
+    private void Move()
+    {
+        // 두 탱크의 평균 지점을 찾는다.
+        FindAveragePosition();
+
+        // transform.position을 m_DesiredPosition으로 m_DampTime 초내 위치시킨다.
+        // m_DampTime 목표에 도달하는데 걸리는 시간
+        transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+    }
+
+
+    private void FindAveragePosition()
+    {
+        Vector3 averagePos = new Vector3();
+        int numTargets = 0;
+
+        // Target => 접속한 플레이어들의 위치로 보면 됨.
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+
+            averagePos += m_Targets[i].position;
+            numTargets++;
+        }
+
+        if (numTargets > 0)
+            averagePos /= numTargets;
+
+        // y값을 실제로는 영향을 미치지 않으므로, transform.position.y로 고정한다.
+        averagePos.y = transform.position.y;
+
+        m_DesiredPosition = averagePos;
+    }
+
+
+    private void Zoom()
+    {
+        float requiredSize = FindRequiredSize();
+        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+    }
+
+
+    private float FindRequiredSize()
+    {
+        // InverseTransformPoint 월드 영역의 정점을 뷰포트 영역의 정점으로 변환한다.
+        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+
+        float size = 0f;
+
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+
+            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
+
+            Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.y));
+
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.x) / m_Camera.aspect);
+        }
+
+        // m_ScreenEdgeBuffer가 0이면 탱크는 화면 가장자리에 걸쳐질 것이다.
+        // 따라서, 약간의 여유분을 주어 화면 내부에 완전히 보이도록 한다.
+        size += m_ScreenEdgeBuffer;
+
+        size = Mathf.Max(size, m_MinSize);
+
+        return size;
+    }
+
+
+    public void SetStartPositionAndSize()
+    {
+        FindAveragePosition();
+
+        transform.position = m_DesiredPosition;
+
+        m_Camera.orthographicSize = FindRequiredSize();
+    }
+}
