699d5ee968733a7046ca416530221bbfd57c7088
batherit <batherit0703@naver.com>
2020-06-07 17:04:21 +0900

    [ADD_SJH] 탄환 발사 추가

---
+++ TeamProject/Assets/SJH/Scripts/SC_TankShooting.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SC_TankShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;
+    public Rigidbody m_Shell;
+    public Transform m_FireTransform;
+    public Slider m_AimSlider;
+    public AudioSource m_ShootingAudio;
+    public AudioClip m_ChargingClip;
+    public AudioClip m_FireClip;
+    public float m_MinLaunchForce = 15f;
+    public float m_MaxLaunchForce = 30f;
+    public float m_MaxChargeTime = 0.75f;
+
+  
+    private string m_FireButton;         
+    private float m_CurrentLaunchForce;  
+    private float m_ChargeSpeed;         
+    private bool m_Fired;                
+
+
+    private void OnEnable()
+    {
+        m_CurrentLaunchForce = m_MinLaunchForce;
+        m_AimSlider.value = m_MinLaunchForce;
+    }
+
+
+    private void Start()
+    {
+        m_FireButton = "Fire" + m_PlayerNumber;
+
+        // m_MaxChargeTime 시간만에 최소힘에서 최대힘에 이르게 된다.
+        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+    }
+    
+
+    private void Update()
+    {
+        // Track the current state of the fire button and make decisions based on the current launch force.
+        m_AimSlider.value = m_MinLaunchForce;
+
+        if(m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        {
+            // 최대치지만, 아직 발사되지 않은 상태
+            m_CurrentLaunchForce = m_MaxLaunchForce;
+            Fire();
+        }
+        else if(Input.GetButtonDown(m_FireButton))
+        {
+            // 발사 버튼을 최초로 누름.
+            m_Fired = false;
+            m_CurrentLaunchForce = m_MinLaunchForce;
+
+            m_ShootingAudio.clip = m_ChargingClip;
+            m_ShootingAudio.Play();
+        }
+        else if(Input.GetButton(m_FireButton) && !m_Fired)
+        {
+            // 발사 버튼을 누르고 있는 상태지만, 아직 발사되지 않음.
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+
+            m_AimSlider.value = m_CurrentLaunchForce;
+        }
+        else if(Input.GetButtonUp(m_FireButton) && !m_Fired)
+        {
+            // 발사 버튼을 뗐지만, 아직 발사되지 않음.
+            Fire();
+        }
+    }
+
+
+    private void Fire()
+    {
+        // Instantiate and launch the shell.
+        m_Fired = true;
+
+        Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+
+        m_ShootingAudio.clip = m_FireClip;
+        m_ShootingAudio.Play();
+
+        m_CurrentLaunchForce = m_MinLaunchForce;
+    }
+}
