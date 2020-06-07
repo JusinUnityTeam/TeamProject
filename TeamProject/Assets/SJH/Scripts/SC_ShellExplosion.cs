69fbcec5d923e9c718a120d09ee5b082315aa8ee
batherit <batherit0703@naver.com>
2020-06-07 15:16:32 +0900

    [ADD_SJH] 탄환 추가

---
+++ TeamProject/Assets/SJH/Scripts/SC_ShellExplosion.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_TankMask;                        // Used to filter what the explosion affects, this should be set to "Players".
+    public ParticleSystem m_ExplosionParticles;         // Reference to the particles that will play on explosion.
+    public AudioSource m_ExplosionAudio;                // Reference to the audio that will play on explosion.
+    public float m_MaxDamage = 100f;                    // The amount of damage done if the explosion is centred on a tank.
+    public float m_ExplosionForce = 1000f;              // The amount of force added to a tank at the centre of the explosion.
+    public float m_MaxLifeTime = 2f;                    // The time in seconds before the shell is removed.
+    public float m_ExplosionRadius = 5f;                // The maximum distance away from the explosion tanks can be and are still affected.
+
+
+    private void Start()
+    {
+        // If it isn't destroyed by then, destroy the shell after it's lifetime.
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // position 위치에서 ExplosionRadius거리 내에 있는 모든 객체 중 m_TankMask에 해당하는 객체 콜라이더를 추출
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+            if (!targetRigidbody) continue;
+
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+            SC_TankHealth targetHealth = targetRigidbody.GetComponent<SC_TankHealth>();
+
+            if (!targetHealth) continue;
+
+            float damage = CalculateDamage(targetRigidbody.position);
+
+            targetHealth.TakeDamage(damage);
+        }
+
+        m_ExplosionParticles.transform.parent = null;
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+        Destroy(gameObject);
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        float explosionDistance = explosionToTarget.magnitude;
+
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+
+        float damage = relativeDistance * m_MaxDamage;
+
+        damage = Mathf.Max(0f, damage);
+
+        return damage;
+    }
+}
